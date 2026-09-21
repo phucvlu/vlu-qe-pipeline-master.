193dccaa06f5e07fd9b296f4cfcdf8fe6c45c5bf
Nguyen Trong Phuc <phuc.2474802010317@vanlanguni.vn>
2026-09-21 14:27:31 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,18 @@
+// auth.js
+function login(username, password) {
+  if (!username) {
+    return false;
+  }
+  
+  if (username === "locked_account") {
+    return false;
+  }
+  
+  if (username === "admin" && password === "123") {
+    return true;
+  }
+  
+  return false;
+}
 
+module.exports = { login };
